c0b1853cfca5a91eb9cb51bcbc482cf30e646732
anastasiya-sadovskaya <sadovskaya.anastasia@gmail.com>
2017-06-07 17:01:13 +0300

    apd

---
+++ aa/client/components/AppManager.js
@@ -0,0 +1,14 @@
+import AppSettings from './AppSettings';
+
+export default (function AppManager() {
+    return{
+        calendar: null,
+        prevMonth: null,
+        currentMonth: null,
+        nextMonth: null,
+        days: [],
+        buttonNext: null,
+        buttonPrev: null,
+        
+    }
+}());
---
+++ aa/client/components/AppSettings.js
@@ -0,0 +1,37 @@
+// import AppManager from './AppManager';
+
+export default (function AppSettings() {
+    var today = new Date();
+        // visibleMonth = today.getMonth(),
+        // visibleYear = today.getFullYear();
+        // firstDayOfVisibleMonth = new Date(visibleYear, visibleMonth, 1).getDay();
+        // lastDayOfVisibleMonth = new Date(visibleYear, visibleMonth, AppSettings.daysInMonth[AppSettings.currentMonth]).getDay(),
+        // lastDaysOfPrevMonthCount = firstDayOfVisibleMonth - 1,
+        // lastDaysOfPrevMonthSinse = AppSettings.daysInMonth[AppSettings.currentMonth - 1] - lastDaysOfPrevMonthCount,
+        // lastDayOfNextMonth = 7 - lastDayOfVisibleMonth;
+
+
+    function isLeapYear(){
+        return today.getFullYear() % 4 == 0;
+    }
+
+    return {
+        today: today,
+        currentDate: today.getDate(),
+        currentDay: today.getDay(),
+        currentMonth: today.getMonth(),
+        currentYear: today.getFullYear(),
+        visibleMonth: today.getMonth(),
+        visibleYear: today.getFullYear(),
+        isLeapYear: isLeapYear(),
+        months: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+        daysInMonth: [31, (isLeapYear() ?29 :28) , 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
+        days: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday','Sunday'],
+        daysShort: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+        // firstDayOfVisibleMonth : firstDayOfVisibleMonth,
+        //     lastDayOfVisibleMonth : lastDayOfVisibleMonth,
+        //     lastDaysOfPrevMonthCount :lastDaysOfPrevMonthCount,
+        //     lastDaysOfPrevMonthSinse : lastDaysOfPrevMonthSinse,
+        //     lastDayOfNextMonth : lastDayOfNextMonth,
+};
+}());
